b99caccd4c115f92dce0253a971419d1b0cff25a
Animehater <notwtf@gmail.com>
2020-03-16 23:51:45 +0200

    Git restructed, 2nd pdf added, 3rd tmp added

---
+++ Homework3/Lab3.java
@@ -0,0 +1,34 @@
+import java.io.*;
+
+public class Lab3{
+	public static boolean advGame(){
+		return True;
+	}
+	public static void testAdv(){
+		int numTrue = 0;
+		int numTries = 3000;
+		try(FileReader reader1 = new FileReader("ecb-distinguish-1.txt")){
+			char c = reader1.read();
+		}
+		catch(IOException){
+			System.out.println(ex.getMessage());
+		}
+
+		try(FileReader reader2 = new FileReader("ecb-distinguish-2.txt")){
+			char c = reader2.read();
+		}
+		catch(IOException){
+			System.out.println(ex.getMessage());
+		}
+
+		for(int i=0; i<numTries; i++){
+			if (advGame()){
+				numTrue++;
+			}
+		}
+	}
+
+	public static void main (String[] args) {
+		testAdv();
+	}
+}
